1b5e1d5dd6dc52b22b8db20ecff3be122f345e07
pedrovargastannuri@gmail.com <pedrovargastannuri@gmail.com>
2024-03-07 18:03:28 -0300

    Lista1

--- DadosAlgebricos.hs
+++ DadosAlgebricos.hs
@@ -1,9 +1,7 @@
 data Semana = Dom | Seg | Ter | Qua | Qui | Sex | Sab
-    deriving (Show, Eq, Ord) -- Fala pro compilador gerar automaticamente uma definição da classe Show, Eq, Ord para o tipo de dado
-
+  deriving (Show, Eq, Ord) -- Fala pro compilador gerar automaticamente uma definição da classe Show, Eq, Ord para o tipo de dado
 
 proximo :: Semana -> Semana
-
 proximo Seg = Ter
 proximo Ter = Qua
 proximo Qua = Qui
@@ -12,34 +10,33 @@ proximo Sex = Sab
 proximo Sab = Dom
 proximo _ = Seg
 
-
 proximo' :: Semana -> Semana
-
 proximo' d =
-    case d of
-        Seg -> Ter
-        Ter -> Qua
-        Qua -> Qui
-        Qui -> Sex
-        Sex -> Sab
-        Sab -> Dom
+  case d of
+    Seg -> Ter
+    Ter -> Qua
+    Qua -> Qui
+    Qui -> Sex
+    Sex -> Sab
+    Sab -> Dom
 
 type Cod = Int
+
 type Nome = String
+
 type Email = String
+
 data Cadastro = Pessoa Cod Nome Email
-    deriving Show
+  deriving (Show)
 
 procurar :: Cod -> [Cadastro] -> Nome
+procurar cod [] = ""
+procurar cod ((Pessoa c n e) : ps)
+  | cod == c = n
+  | otherwise = procurar cod ps
 
-procurar cod [] = "" 
-procurar cod ((Pessoa c n e):ps)
-    | cod == c = n
-    | otherwise = procurar cod ps
-
-    
-data Dupla a b = Par a b --Dupla não é um tipo, mas sim um construtor de tipo
-    deriving Show
+data Dupla a b = Par a b -- Dupla não é um tipo, mas sim um construtor de tipo
+  deriving (Show)
 
 primeiro :: Dupla a b -> a
 primeiro (Par a _) = a
@@ -47,32 +44,30 @@ primeiro (Par a _) = a
 segundo :: Dupla a b -> b
 segundo (Par _ b) = b
 
+data Lista a = a :> (Lista a) | Nil
+  deriving (Show)
 
-data Lista a = Cons a (Lista a) | Nil
-    deriving Show
-
-tamanho :: Num b => Lista a -> b
+tamanho :: (Num b) => Lista a -> b
 tamanho Nil = 0
-tamanho (Cons a l) = 1 + tamanho l
+tamanho (a :> l) = 1 + tamanho l
 
 data Arvore a = Folha | No a (Arvore a) (Arvore a)
-    deriving Show
+  deriving (Show)
 
-ins :: Ord a => a -> Arvore a -> Arvore a
+ins :: (Ord a) => a -> Arvore a -> Arvore a
 ins n Folha = No n Folha Folha
-ins n a@(No x e d) --Esse @ indica que a é equivalente a (No x e d)
-    | n > x = No x e (ins n d)
-    | n < x = No x (ins n e) d
-    | otherwise = a
+ins n a@(No x e d) -- Esse @ indica que 'a' é equivalente a (No x e d)
+  | n > x = No x e (ins n d)
+  | n < x = No x (ins n e) d
+  | otherwise = a
 
-
-altura :: Ord b => Num b => Arvore a -> b
+altura :: (Ord b) => (Num b) => Arvore a -> b
 altura Folha = 0
 altura (No a e d) = 1 + max (altura e) (altura d)
 
---Exercicio: funcao que recebe uma arvore e retorna uma lista ordenada 
+-- Exercicio: funcao que recebe uma arvore e retorna uma lista ordenada
 
-arvoreParaLista :: Arvore a -> [a]
-arvoreParaLista Folha = []
-arvoreParaLista (No a e d) = arvoreParaLista e ++ [a] ++ arvoreParaLista d
+inOrder :: Arvore a -> [a]
+inOrder Folha = []
+inOrder (No a e d) = inOrder e ++ [a] ++ inOrder d
 
