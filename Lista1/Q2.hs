f5be46cf49343d32e6fcd29e03608753840c781f
dipled <helenavargastannuri@gmail.com>
2024-03-09 08:45:08 -0300

    arrumando coisas

--- Lista1/Q2.hs
+++ Lista1/Q2.hs
@@ -4,7 +4,7 @@ import qualified Q1
 data RB = R | B
   deriving (Show)
 
-data RBTree a = Node a RB (RBTree a) (RBTree a) | Leaf RB
+data RBTree a = Node a RB (RBTree a) (RBTree a) | Leaf
   deriving (Show)
 
 rot :: RBTree a -> RBTree a
@@ -15,12 +15,12 @@ rot (Node z B a (Node y R (Node x R b c) d)) = Node y R (Node x B a b) (Node z B
 rot n = n
 
 rootColor :: RBTree a -> RBTree a
-rootColor (Leaf _) = Leaf B 
+rootColor Leaf = Leaf
 rootColor (Node x R l r) = Node x B l r 
 rootColor n@(Node x B l r ) = n 
 
 insertNode' :: (Ord a) => a -> RBTree a -> RBTree a
-insertNode' e (Leaf c) = Node e R (Leaf B) (Leaf B)
+insertNode' e Leaf = Node e R Leaf Leaf
 insertNode' e n@(Node x c l r)
   |e == x = n
   |e > x = rot (Node x c l (insertNode' e r))
@@ -32,5 +32,5 @@ insertNode e a = rootColor(rot (insertNode' e a))
 main :: IO()
 main =
   do
-    let a = insertNode 9 (insertNode 10 (insertNode 13 (insertNode 12 (insertNode 11 (Leaf B)))))
+    let a = insertNode 9 (insertNode 10 (insertNode 13 (insertNode 12 (insertNode 11 Leaf))))
     print a
